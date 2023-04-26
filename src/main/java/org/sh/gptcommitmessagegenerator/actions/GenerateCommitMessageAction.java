a7a2a8660ed7994cbf6b90bfe24867a45a52f0f7
shabdan <sh.abjaparov@globerce.com>
2023-04-26 11:37:37 +0600

    feat(project): added base project structure

--- src/main/java/org/sh/gptcommitmessagegenerator/actions/GenerateCommitMessageAction.java
+++ src/main/java/org/sh/gptcommitmessagegenerator/actions/GenerateCommitMessageAction.java
@@ -3,53 +3,27 @@ package org.sh.gptcommitmessagegenerator.actions;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-public class GitCommitAction extends AnAction {
+public class GenerateCommitMessageAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
-        Project project = event.getProject();
+        final Project project = event.getProject();
         if (project == null) {
             return;
-
-
-
         }
 
-        String commitMessage = generateCommitMessage();
+        final String commitMessage = generateCommitMessage();
         if (commitMessage == null) {
             return;
         }
-
-        Messages.showInfoMessage(project, "Generated commit message:\n\n" + commitMessage, "Commit Message");
-    }
+        final ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(project);
+        final ToolWindow toolWindow = toolWindowManager.getToolWindow("Vcs.Commit.PrimaryCommitActions");
+//        toolWindow.getContentManager().addContent();
+    }//TODO add functionality to edit commit message
 
     private String generateCommitMessage() {
-        try {
-            ProcessBuilder processBuilder = new ProcessBuilder("git", "commit");
-            Process process = processBuilder.start();
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            StringBuilder output = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                output.append(line).append("\n");
-            }
-
-            int exitCode = process.waitFor();
-            if (exitCode == 0) {
-                return output.toString();
-            } else {
-                return null;
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-            return null;
-        }
+        return "feat(): Sample generated message";
     }
 }
